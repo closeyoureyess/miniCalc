6599173c46c4ad406befe8f13b2100cff570b61d
mxnv <vadim.beller@mail.ru>
2024-05-03 01:58:13 +0300

    #7 commit

--- myProject/src/Main.java
+++ myProject/src/Main.java
@@ -36,8 +36,10 @@ public class Main {
                 numbertwo = scanner.nextInt();
                 System.out.println(mathOperations.substractionMethod(numberone, numbertwo));
             }
+            else{
+                System.out.println("Ошибка!");
+            }
             break;
         }
-
     }
 }
