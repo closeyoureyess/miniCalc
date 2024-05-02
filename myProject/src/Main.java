8d46dc304e11a1d1ee4d93dd7c777d72f2988819
mxnv <vadim.beller@mail.ru>
2024-05-03 01:28:44 +0300

    #1 commit

---
+++ myProject/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int numberone;
+        int numbertwo;
+        //int multiplication;
+        String typeOperations;
+        MathOperations mathOperations = new MathOperations();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Введите тип операции: ");
+            typeOperations = scanner.nextLine();
+            if (typeOperations.equals("*")) {
+                System.out.println("Введите первое число: ");
+                numberone = scanner.nextInt();
+                System.out.println("Введите второе число: ");
+                numbertwo = scanner.nextInt();
+                System.out.println(mathOperations.multiplicationMethod(numberone, numbertwo));
+            } else if (typeOperations.equals("+")) {
+                System.out.println("Введите первое число: ");
+                numberone = scanner.nextInt();
+                System.out.println("Введите второе число: ");
+                numbertwo = scanner.nextInt();
+                System.out.println(mathOperations.sumMethod(numberone, numbertwo));
+            } else if (typeOperations.equals(":")) {
+                System.out.println("Введите первое число: ");
+                numberone = scanner.nextInt();
+                System.out.println("Введите второе число: ");
+                numbertwo = scanner.nextInt();
+                System.out.println(mathOperations.divisionMethod(numberone, numbertwo));
+            } else if (typeOperations.equals("-")) {
+                System.out.println("Введите первое число: ");
+                numberone = scanner.nextInt();
+                System.out.println("Введите второе число: ");
+                numbertwo = scanner.nextInt();
+                System.out.println(mathOperations.substractionMethod(numberone, numbertwo));
+            }
+            break;
+        }
+    }
+}
---
+++ myProject/src/MathOperations.java
@@ -0,0 +1,15 @@
+public class MathOperations {
+    public int multiplicationMethod(int once, int twice){
+        return once * twice;
+    }
+    public int sumMethod(int once, int twice){
+        return once + twice;
+    }
+    public int substractionMethod(int once, int twice){
+        return once - twice;
+    }
+    public int divisionMethod(int once, int twice){
+        return once/twice;
+    }
+
+}
